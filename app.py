02f8afd3329ea5a9747870174873ab08a767c2ca
Abdul Rehman <supermind2002@gmail.com>
2023-07-23 00:24:03 +0500

    version route added

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+import cv2
+import numpy as np
+import sklearn
+from flask import Flask, send_from_directory, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    print('Request for index page received')
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -18,15 +18,28 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return render_template('hello.html', name=name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
+@app.route('/versions', methods=['GET'])
+def versions():
+    opencv_version = cv2.__version__
+    sklearn_version = sklearn.__version__
+    numpy_version = np.__version__
+
+    versions_dict = {
+        "opencv_version": opencv_version,
+        "scikit-learn_version": sklearn_version,
+        "numpy_version": numpy_version
+    }
+
+    return jsonify(versions_dict)
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
