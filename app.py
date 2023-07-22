c1528b26ecea1ccb88cc3fe0e6fba0a8305398ab
Abdul Rehman <abdul.rehman.vud@gmail.com>
2023-07-23 02:20:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
-import json
-import cv2
-import numpy as np
-import sklearn
+
+
 
 
 from flask import (Flask, redirect, render_template, request,
@@ -32,20 +30,6 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
-
-@app.route('/versions', methods=['GET'])
-def versions():
-    opencv_version = cv2.__version__
-    sklearn_version = sklearn.__version__
-    numpy_version = np.__version__
-
-    versions_dict = {
-        "opencv_version": opencv_version,
-        "scikit-learn_version": sklearn_version,
-        "numpy_version": numpy_version
-    }
-
-    return jsonify(versions_dict)       
 
 
 if __name__ == '__main__':
